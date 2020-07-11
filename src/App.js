7bbf4067f5451d3de5e7842861ffde2be9fbaf7c
Herry Roby <herry.roby@gmail.com>
2020-07-11 14:18:41 +0700

    main

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import IncrDecrClass from './components/pages/CounterCC';
+import IncrDecrFC from './components/pages/CounterFC';
+// import Qtemu from './components/pages/Qtemu';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    {/* <QtemuPage /> */}
+    <IncrDecrClass />
+    <IncrDecrFC />
+  </>
+);
 
 export default App;
